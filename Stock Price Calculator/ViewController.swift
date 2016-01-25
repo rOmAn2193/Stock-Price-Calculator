4b595abb02c83cd00f3b4f57fe4503865abcf219
rOmAn2193 <roman_puzey@aol.com>
2016-01-24 23:17:56 -0700

    first commit

--- Stock Price Calculator/ViewController.swift
+++ Stock Price Calculator/ViewController.swift
@@ -8,7 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate
+{
+
+    @IBOutlet weak var txtOriginalPrice: UITextField!
+    @IBOutlet weak var txtAmountBought: UITextField!
+    @IBOutlet weak var txtSellingPrice: UITextField!
+
+
+    @IBOutlet weak var lblPercentIncrease: UILabel!
+    @IBOutlet weak var lblProfit: UILabel!
+
+    var originalPrice = ""
+    var amountBought = ""
+    var sellingPrice = ""
+
+    var percentIncrease : Float = 0
+    var profit : Float = 0
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +37,65 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnCalculateACTION(sender: UIButton)
+    {
+        takeInData()
+        calculatePrice()
+    }
 
+    @IBAction func btnClearACTION(sender: UIButton)
+    {
+        clear()
+    }
+
+    func takeInData()
+    {
+        originalPrice = txtOriginalPrice.text!
+        amountBought = txtAmountBought.text!
+        sellingPrice = txtSellingPrice.text!
+    }
+
+    func calculatePrice()
+    {
+        var fOriginalPrice = Float(originalPrice)
+        var fAmountBought = Float(amountBought)
+        var fSellingPrice = Float(sellingPrice)
+
+        percentIncrease = (fSellingPrice! / fOriginalPrice! - 1) * 100
+        profit = (fSellingPrice! * fAmountBought!) - (fOriginalPrice! * fAmountBought!)
+
+        printProfit()
+    }
+
+    func clear()
+    {
+        txtAmountBought.text = ""
+        txtOriginalPrice.text = ""
+        txtSellingPrice.text = ""
+
+        lblPercentIncrease.text = "Percent Increase"
+        lblProfit.text = "Profit"
+    }
+
+    func printProfit()
+    {
+        var sPercent = String(format: "%0.2f", percentIncrease)
+        var sProfit = String(format: "%0.2f", profit)
+
+        lblPercentIncrease.text = "\(sPercent)%"
+        lblProfit.text = "$\(sProfit)"
+    }
+
+    func hideTheKeyboard()
+    {
+        txtAmountBought.resignFirstResponder()
+        txtOriginalPrice.resignFirstResponder()
+        txtSellingPrice.resignFirstResponder()
+    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
+        hideTheKeyboard()
+    }
 }
 
